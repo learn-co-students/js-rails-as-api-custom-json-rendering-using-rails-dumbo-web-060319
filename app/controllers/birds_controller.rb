1574f742759b19f185cc7ebc26786813680c7c09
github username <github email address>
2019-07-26 13:23:49 -0400

    READ README

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,14 @@
+
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+    # render json: birds, except: [:created_at, :updated_at]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    #bird = Bird.find(params[:id])
+    render json: bird, only: [:id, :name, :species]
   end
 end
